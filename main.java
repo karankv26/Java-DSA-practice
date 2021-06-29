508bb1fa03f039295bd533f4d8f55a342a35f247
Karan K V <62898070+karankv26@users.noreply.github.com>
2021-06-30 01:48:09 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) {
+        int n = 30;
+        if (n >= 21) {
+            System.out.println("Hello World");
+        } else {
+            System.out.println("no");
+        }
+    }
+
+}
