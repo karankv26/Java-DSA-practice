6f1ee4a511fdb28d51794d3592a73cdfd0c86f0f
Karan K V <62898070+karankv26@users.noreply.github.com>
2021-07-19 23:49:15 +0530

    Create GCD.java

---
+++ Assignment 2/GCD.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class karan {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the 1st number: ");
+        int n1 = scanner.nextInt();
+        System.out.print("Enter the 2nd number: ");
+        int n2 = scanner.nextInt();
+        int gcd = 1;
+        for (int i = 1; i <= n1 && i <= n2; i++) {
+            if (n1 % i == 0 && n2 % i == 0)
+                gcd = i;
+        }
+      
+        System.out.printf("LCM of %d and %d is %d\n", n1, n2, Lcm);
+    }
+}
