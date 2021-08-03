77e271a1d0e3740049607084c1e1285e7db9c038
Karan K V <62898070+karankv26@users.noreply.github.com>
2021-08-03 17:10:35 +0530

    Create multiplicationtable.java

---
+++ multiplicationtable.java
@@ -0,0 +1,17 @@
+import java.lang.annotation.Target;
+import java.util.*;
+
+public class karan {
+
+
+    public static void main(String[] args){  
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");      
+        int num = sc.nextInt();
+
+        for(int i=1; i <= 10; i++)  
+        {      
+        System.out.println(num+" * "+i+" = "+num*i);  
+        }  
+    }
+}
