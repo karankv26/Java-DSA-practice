e4c1e8d40fb2bbf94ba858ffeb9016fa7c130249
Karan K V <62898070+karankv26@users.noreply.github.com>
2021-08-15 13:00:37 +0530

    Create Batting_average.java

---
+++ Kunal course/Batting_average.java
@@ -0,0 +1,27 @@
+package com.DSA;
+
+import java.util.Scanner;
+
+public class batting_avg {
+    public static void main(String[] args){
+        System.out.println("Enter the score: ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("Enter the number of wickets: ");
+        int b = sc.nextInt();
+        int result = a/b;
+        if (result < 10){
+            System.out.println(result+",poor batting skills!");
+        }else if (result >= 10 && result <20){
+            System.out.println(result+",Average batting skills!");
+        }else if (result >= 20 && result <30){
+            System.out.println(result+",Above average batting skills!");
+        }else if (result >= 30 && result <40){
+            System.out.println(result+",good batting skills!");
+        }else if (result >= 40 && result <50){
+            System.out.println(result+",very good batting skills!");
+        }else{
+            System.out.println(result+",extraordinary batting skills!");
+        }
+    }
+}
